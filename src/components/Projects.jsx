474efb1ee52d4717ddc5d029b720e3b3c6760a60
vinayakanvs <vinayakanvs2003@gmail.com>
2024-02-02 19:21:05 +0530

    added pagination in project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,32 @@
-
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 function Projects() {
+
+  const [currentPage, setCurrentPage] = useState(0); 
+  const projectsPerPage = 3;
+
+  const projects = [
+    { id: 1, title: 'Project 1', imageUrl: './assets/project_chattersphere.png' },
+    { id: 2, title: 'Project 2', imageUrl: './assets/project_chattersphere.png' },
+    { id: 3, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
+    { id: 3, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
+    { id: 3, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
+    { id: 3, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
+    { id: 3, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
+  ];
+
+  const pageCount = Math.ceil(projects.length / projectsPerPage);
+
+  const displayedProjects = projects.slice(
+    currentPage * projectsPerPage,
+    (currentPage + 1) * projectsPerPage
+  );
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
   return (
     <section className="project">
       <div className="container">
@@ -8,8 +34,33 @@ function Projects() {
         <h1>Projects</h1>
         <p>Come on in and check out my projects! They're not just lines of code or designs – they're the result of my love for solving problems and getting creative. Let's take a peek together into the world where my skills and ideas collide.</p>
         <div className="project-list">
-          <div className="card"></div>
+          {/* <div className="card"></div> */}
+          {/* Render your displayed projects here */}
+          {displayedProjects.map((project) => (
+            <div className="card" key={project.id}>
+              <img
+                src={project.imageUrl}
+                alt={project.title}
+                style={{ width: '100%', height: '100%', borderRadius: '15px' }}
+              />
+              <div className="project-info">
+                <h4>{project.title}</h4>
+              </div>
+            </div>
+          ))}
         </div>
+        {/* Pagination component */}
+        <ReactPaginate
+          previousLabel={'Previous'}
+          nextLabel={'Next'}
+          breakLabel={'...'}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageChange}
+          containerClassName={'pagination'}
+          activeClassName={'active'}
+        />
       </div>
     </section>
   )
