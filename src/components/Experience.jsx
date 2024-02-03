31298787617886fdd42f0885dbf13ddff54cb58c
vinayakanvs <vinayakanvs2003@gmail.com>
2024-02-03 23:12:16 +0530

    experience section ground done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ function Experience() {
         <div className="container">
             <div className="info">
                 <h1 className="title">Experience</h1>
-                <p>Come on in and check out my projects! They&apos;re not just lines of code or designs – they&apos;re the result of my love for solving problems and getting creative. Let&apos;s take a peek together into the world where my skills and ideas collide.</p>
+                <p>Hey there! I&apos;m on a journey of creativity and exploration. Come take a stroll through my experiences &ndash; each project is like a chapter in my story of learning and making cool things happen. Excited to share the ride with you!</p>
             </div>
             <div className="timeline-container">
                 <VerticalTimeline>
@@ -15,6 +15,7 @@ function Experience() {
                         contentStyle={{background:'#1d1836',color:"#fff"}}
                         contentArrowStyle={{borderRight:"7px solid #232631"}}
                         date="March 2020 - April 2021"
+                        iconStyle={{background:"#E6DEDD"}}
                         icon={
                             <div className="company_logo_container">
                                 <img 
@@ -25,8 +26,14 @@ function Experience() {
                             </div>
                         }
                     >
-                        <div>
-                            <h3>Web Developer</h3>
+                        <div className="description-container">
+                            <h3 className="job-title">Web Developer Intern</h3>
+                            <p className="company-name">Einsicht Technologies</p>
+                            <ul>
+                                <li> Collaborated with two other interns to create a culturally tailored, responsive login/signup interface.</li>
+                                <li>Utilized HTML, CSS, JavaScript, and jQuery to implement responsive and user-friendly authentication interfaces, enhancing user experience and security</li>
+                                <li>Gained exposure to Figma for design mockups and incorporated HTML code within PHP scripts, broadening my understanding of web development frameworks.</li>
+                            </ul>
                         </div>
                     </VerticalTimelineElement>
                 </VerticalTimeline>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import ReactPaginate from 'react-paginate';
 
 function Projects() {
