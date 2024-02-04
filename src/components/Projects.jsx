adba3b6c5161ac5ce32d44420c87cc9b9b4059ba
vinayakanvs <vinayakanvs2003@gmail.com>
2024-02-04 23:25:24 +0530

    fixed project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import ReactPaginate from 'react-paginate';
-
+import project_gre from '../assets/project_gre.png'
+import project_chat from '../assets/project_chattersphere.png'
+import project_chess from '../assets/project_chess.png'
 function Projects() {
 
   const [currentPage, setCurrentPage] = useState(0); 
   const projectsPerPage = 3;
 
   const projects = [
-    { id: 1, title: 'Project 1', imageUrl: './assets/project_chattersphere.png' },
-    { id: 2, title: 'Project 2', imageUrl: './assets/project_chattersphere.png' },
-    { id: 3, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
+    { id: 1, title: 'Project 1', imageUrl: project_chat },
+    { id: 2, title: 'Project 2', imageUrl: project_gre },
+    { id: 3, title: 'Project 3', imageUrl: project_chess },
     { id: 4, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
     { id: 5, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
     { id: 6, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
@@ -40,10 +42,11 @@ function Projects() {
         <div className="project-list">
           {displayedProjects.map((project) => (
             <div className="card" key={project.id}>
+              <img src={`${project.imageUrl}`} alt="" />
               <div className="overlay">
                 <div className="project-buttons">
                   <a href={project.link1} target="_blank" rel="noopener noreferrer">GitHub</a>
-                  <a href={project.link2} target="_blank" rel="noopener noreferrer">Live Website</a>
+                  <a href={project.link2} target="_blank" rel="noopener noreferrer">Demo</a>
                 </div>
               </div>
             </div>
