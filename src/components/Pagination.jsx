f0262af2d9e343456f57919f49cd0787e1388bd2
vinayakanvs <vinayakanvs2003@gmail.com>
2024-02-05 18:30:33 +0530

    custom built pagination implemented

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+function Pagination({totalProjects,projectsPerPage,setCurrentPage,currentPage}) {
+    let pages = [];
+
+    for(let i = 1;i<=Math.ceil(totalProjects/projectsPerPage);i++){
+        pages.push(i)
+    }
+  return (
+    <div className='pagination'>
+        {
+            pages.map((page,index)=>{
+                return <button 
+                            key={index} 
+                            className={page==currentPage?'active':''}
+                            onClick={()=> setCurrentPage(page)}>{page}
+                        </button>
+            })
+        }
+    </div>
+  )
+}
+
+Pagination.propTypes = {
+    totalProjects: PropTypes.number.isRequired,
+    projectsPerPage: PropTypes.number.isRequired,
+    setCurrentPage: PropTypes.func.isRequired,
+    currentPage: PropTypes.number.isRequired,
+  };
+
+export default Pagination
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,69 +1,43 @@
 import { useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import project_gre from '../assets/project_gre.png'
-import project_chat from '../assets/project_chattersphere.png'
-import project_chess from '../assets/project_chess.png'
+import projects from '../resources/Info';
+import Pagination from './Pagination';
+
 function Projects() {
 
-  const [currentPage, setCurrentPage] = useState(0); 
-  const projectsPerPage = 3;
-
-  const projects = [
-    { id: 1, title: 'Project 1', imageUrl: project_chat },
-    { id: 2, title: 'Project 2', imageUrl: project_gre },
-    { id: 3, title: 'Project 3', imageUrl: project_chess },
-    { id: 4, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
-    { id: 5, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
-    { id: 6, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
-    { id: 7, title: 'Project 3', imageUrl: './assets/project_chattersphere.png' },
-  ];
-
-  const pageCount = Math.ceil(projects.length / projectsPerPage);
-
-  const displayedProjects = projects.slice(
-    currentPage * projectsPerPage,
-    (currentPage + 1) * projectsPerPage
-  );
-
-  const handlePageChange = ({ selected }) => {
-    setCurrentPage(selected);
-  };
-
+  const [currentPage,setCurretPage] = useState(1)
+  // eslint-disable-next-line no-unused-vars
+  const [projectsPerPage, _setProjectsPerPage] = useState(3)
   
-  
-
+  const lastProjectIndex = currentPage * projectsPerPage
+  const firstProjectIndex = lastProjectIndex - projectsPerPage;
+  const currentProjects = projects.slice(firstProjectIndex,lastProjectIndex)
 
   return (
-    <section className="project" >
+    <section className="project">
       <div className="container">
         <h3>MY WORK</h3>
         <h1>Projects</h1>
         <p>Come on in and check out my projects! They&apos;re not just lines of code or designs – they&apos;re the result of my love for solving problems and getting creative. Let&apos;s take a peek together into the world where my skills and ideas collide.</p>
         <div className="project-list">
-          {displayedProjects.map((project) => (
-            <div className="card" key={project.id}>
-              <img src={`${project.imageUrl}`} alt="" />
-              <div className="overlay">
-                <div className="project-buttons">
-                  <a href={project.link1} target="_blank" rel="noopener noreferrer">GitHub</a>
-                  <a href={project.link2} target="_blank" rel="noopener noreferrer">Demo</a>
-                </div>
-              </div>
+        {currentProjects.map((project) => (
+          <div className="card" key={project.id}>
+          <img src={`${project.imageUrl}`} alt="" />
+          <div className="overlay">
+            <div className="project-buttons">
+              <a href={projects.link1} target="_blank" rel="noopener noreferrer">GitHub</a>
+              <a href={projects.link2} target="_blank" rel="noopener noreferrer">Demo</a>
             </div>
-          ))}
+          </div>
         </div>
-        {/* Pagination component */}
-        <ReactPaginate
-          previousLabel={'Previous'}
-          nextLabel={'Next'}
-          breakLabel={'...'}
-          pageCount={pageCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={handlePageChange}
-          containerClassName={'pagination'}
-          activeClassName={'active'}
+        ))}
+        </div>
+        <Pagination
+          totalProjects={projects.length}
+          projectsPerPage={projectsPerPage}
+          setCurrentPage={setCurretPage}
+          currentPage={currentPage}
         />
+        
       </div>
     </section>
   )
