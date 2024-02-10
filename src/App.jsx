3761b316e2105a0fd9e5c8fd231c06e619ee179e
vinayakanvs <vinayakanvs2003@gmail.com>
2024-02-11 00:53:56 +0530

    navbar properly functional

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     await console.log(container);
   }, []);
 
-  
+  const [currentTab, setCurrentTab] = useState("about");
 
   useEffect(() => {
     const handleScroll = () => {
@@ -60,97 +60,140 @@ const App = () => {
     };
   }, []);
 
-    return (
-      <div className="particle-container">
-        <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "transparent",
-                    },
-                },
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 4,
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: "#ffffff",
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 2,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 20,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 5 },
-                    },
-                },
-                detectRetina: true,
-            }}
-        />
-        <nav>
-          <ul>
-            <li>About</li>
-            <li>Project</li>
-            <li>Experience</li>
-            <li>Education</li>
-            <li>Achievements</li>
-          </ul>
-        </nav>
-        <About/>
-        <Projects/>
-        <Experience/>
-        <Contact/>
-      </div>
-    );
+  useEffect(() => {
+    const handleScroll = () => {
+      const aboutSection = document.getElementById("about-me");
+      const projectSection = document.getElementById("project");
+      const experienceSection = document.getElementById("experience");
+      const contactSection = document.getElementById("contact");
+
+      const scrollPosition = window.scrollY + 200; // Offset to account for navbar height or other factors
+
+      if (
+        scrollPosition >= aboutSection.offsetTop &&
+        scrollPosition < projectSection.offsetTop
+      ) {
+        setCurrentTab("about");
+      } else if (
+        scrollPosition >= projectSection.offsetTop &&
+        scrollPosition < experienceSection.offsetTop
+      ) {
+        setCurrentTab("projects");
+      } else if (
+        scrollPosition >= experienceSection.offsetTop &&
+        scrollPosition < contactSection.offsetTop
+      ) {
+        setCurrentTab("experience");
+      } else if (scrollPosition >= contactSection.offsetTop) {
+        setCurrentTab("contact");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="particle-container">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 20,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <nav>
+        <ul>
+          <li>
+            <a href="#about-me" className={currentTab === "about" ? "active" : ""}>About</a>
+          </li>
+          <li>
+            <a href="#project" className={currentTab === "projects" ? "active" : ""}>Projects</a>
+          </li>
+          <li>
+            <a href="#experience" className={currentTab === "experience" ? "active" : ""}>Experience</a>
+          </li>
+          <li>
+            <a href="#contact" className={currentTab === "contact" ? "active" : ""}>Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <About />
+      <Projects />
+      <Experience />
+      <Contact />
+    </div>
+  );
 };
 
 export default App;
