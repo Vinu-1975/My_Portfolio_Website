aa68f85057adefbb12cc5676ccc6a97eacdc0413
vinayakanvs <vinayakanvs2003@gmail.com>
2024-02-11 23:46:55 +0530

    properly defined projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,46 +1,73 @@
-import { useState } from 'react';
-import projects from '../resources/Info';
-import Pagination from './Pagination';
+import { useState } from "react";
+import projects from "../resources/Info";
+import Pagination from "./Pagination";
 
 function Projects() {
-
-  const [currentPage,setCurretPage] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1);
   // eslint-disable-next-line no-unused-vars
-  const [projectsPerPage, _setProjectsPerPage] = useState(2)
-  
-  const lastProjectIndex = currentPage * projectsPerPage
+  const [projectsPerPage, _setProjectsPerPage] = useState(2);
+
+  const lastProjectIndex = currentPage * projectsPerPage;
   const firstProjectIndex = lastProjectIndex - projectsPerPage;
-  const currentProjects = projects.slice(firstProjectIndex,lastProjectIndex)
+  const currentProjects = projects.slice(firstProjectIndex, lastProjectIndex);
 
   return (
-    <section className="project" id='project'>
+    <section className="project" id="project">
       <div className="container">
         <h1>Projects</h1>
-        <p>Come on in and check out my projects! They&apos;re not just lines of code or designs – they&apos;re the result of my love for solving problems and getting creative. Let&apos;s take a peek together into the world where my skills and ideas collide.</p>
+        <p>
+          Come on in and check out my projects! They&apos;re not just lines of
+          code or designs – they&apos;re the result of my love for solving
+          problems and getting creative. Let&apos;s take a peek together into
+          the world where my skills and ideas collide.
+        </p>
         <div className="project-list">
-        {currentProjects.map((project) => (
-          <div className="card" key={project.id}>
-          <img src={`${project.imageUrl}`} alt="" />
-          <div className="overlay">
-            <div className="project-buttons">
-              <a href={projects.link1} target="_blank" rel="noopener noreferrer">GitHub</a>
-              <a href={projects.link2} target="_blank" rel="noopener noreferrer">Demo</a>
+          {currentProjects.map((project) => (
+            <div className="card-container" key={project.id}>
+              <div className="card">
+                <img src={`${project.imageUrl}`} alt="" />
+                <div className="overlay">
+                  <div className="project-buttons">
+                    <a
+                      href={projects.link1}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      GitHub
+                    </a>
+                    <a
+                      href={projects.link2}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Demo
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="project-info">
+                {(project.about)&& project.about}
+              </div>
+              <div className="tech-stack">
+                {project.technologies && project.technologies.map((tech,i)=>(
+                  <div className="tech" key={i} style={{color:tech.color}}>
+                    {"#"+tech.tech}
+                  </div>
+                ))}
+              </div>
+              {/* {console.log(project.technologies)} */}
             </div>
-          </div>
-        </div>
-        ))}
+          ))}
         </div>
         <Pagination
           totalProjects={projects.length}
           projectsPerPage={projectsPerPage}
-          setCurrentPage={setCurretPage}
+          setCurrentPage={setCurrentPage}
           currentPage={currentPage}
         />
-        
       </div>
     </section>
-  )
+  );
 }
 
-export default Projects
-
+export default Projects;
--- src/resources/Info.js
+++ src/resources/Info.js
@@ -1,15 +1,96 @@
-import project_gre from '../assets/project_gre.png'
-import project_chat from '../assets/project_chattersphere.png'
-import project_chess from '../assets/project_chess.png'
-import project_railway from '../assets/project_railway.jpeg'
-import project_learn4u from '../assets/project_learn4u.png'
+import project_gre from "../assets/project_gre.png";
+import project_chat from "../assets/project_chattersphere.png";
+import project_chess from "../assets/project_chess.png";
+import project_railway from "../assets/project_railway.jpeg";
+import project_learn4u from "../assets/project_learn4u.png";
+
+const techs = { 
+  "React": "skyblue",
+  "Expressjs": "darkred",
+  "Nodejs": "green",
+  "MongoDB": "lightgreen",
+  "Socket.io":"white",
+  "HTML": "orangered",
+  "CSS": "royalblue",
+  "JavaScript": "yellow",
+  "jQuery": "darkblue",
+  "PHP": "deepskyblue",
+  "MySQL": "darkslategrey",
+  "XAMPP": "darkgoldenrod"
+}
 
 const projects = [
-    { id: 1, title: 'ChatterSphere', imageUrl: project_chat },
-    { id: 2, title: 'GreInsights', imageUrl: project_gre },
-    { id: 3, title: 'CheckMate Academy', imageUrl: project_chess },
-    { id: 4, title: 'Railway Management System', imageUrl: project_railway },
-    { id: 5, title: 'Learn4U', imageUrl: project_learn4u },
-  ];
+  { 
+    id: 1, 
+    title: "ChatterSphere", 
+    imageUrl: project_chat, 
+    about:"Modern chat app with real-time communication, account creation, profile management, group interactions, user search, group discussions, and image personalization.",
+    technologies:[
+      {
+        tech:'React',
+        color:techs.React
+      },
+      {
+        tech:'Expressjs',
+        color:techs.Expressjs
+      },
+      {
+        tech:'MongoDB',
+        color:techs.MongoDB
+      },
+      {
+        tech:'Nodejs',
+        color:techs.Nodejs
+      },
+      {
+        tech:'Socket.io',
+        color:techs["Socket.io"]
+      }
+    ]
+  },
+  { 
+    id: 2, 
+    title: "GreInsights", 
+    imageUrl: project_gre ,
+    about:"app analyzes GRE scores, offers Ivy League recommendations based on 5-year admission trends. Ideal for college-bound students seeking guidance.",
+    technologies:[
+      {
+        tech:'HTML5',
+        color:techs.HTML
+      },
+      {
+        tech:'CSS3',
+        color:techs.CSS
+      },
+      {
+        tech:'MySQL',
+        color:techs.MySQL
+      },
+      {
+        tech:'pHp',
+        color:techs.PHP
+      },
+      {
+        tech:'XAMPP',
+        color:techs.XAMPP
+      }
+    ]
+  },
+  { 
+    id: 3, 
+    title: "CheckMate Academy", 
+    imageUrl: project_chess 
+  },
+  { 
+    id: 4, 
+    title: "Railway Management System", 
+    imageUrl: project_railway 
+  },
+  { 
+    id: 5, 
+    title: "Learn4U", 
+    imageUrl: project_learn4u 
+  },
+];
 
-  export default projects
+export default projects;
