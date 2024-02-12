867b3bf4476a911f5ff2fcfbc7980293ada6989d
vinayakanvs <vinayakanvs2003@gmail.com>
2024-02-12 12:12:31 +0530

    added more projects

--- src/resources/Info.js
+++ src/resources/Info.js
@@ -3,6 +3,9 @@ import project_chat from "../assets/project_chattersphere.png";
 import project_chess from "../assets/project_chess.png";
 import project_railway from "../assets/project_railway.jpeg";
 import project_learn4u from "../assets/project_learn4u.png";
+import project_portfolio from "../assets/project_portfolio.png"
+import project_crop from "../assets/project_crop.png"
+import project_battleship from "../assets/project_battleship.jpg"
 
 const techs = { 
   "React": "skyblue",
@@ -20,7 +23,14 @@ const techs = {
   "Python": "darkgreen",
   "Pygame": "orangered",
   "Stockfish": "darkcyan",
-  "Java": "darkorange"
+  "Java": "darkorange",
+  "Framer Motion": "deepskyblue",
+  "Vite": "dodgerblue",
+  "MicroPython": "darkseagreen",
+  "Pico": "lightcoral",
+  "DHT11": "lightskyblue",
+  "pH sensor": "mediumorchid",
+  "C":"darkslategray"
 }
 
 const projects = [
@@ -52,8 +62,75 @@ const projects = [
       }
     ]
   },
+  {
+    id: 2,
+    title: "Smart Crop Prediction System",
+    imageUrl:project_crop,
+    about:"This project utilizes machine learning for real-time crop suggestions, enhancing agricultural decision-making with accurate predictions based on environmental factors.",
+    technologies:[
+      {
+        tech:'MicroPython',
+        color:techs.MicroPython
+      },
+      {
+        tech:'Pico',
+        color:techs.Pico
+      },
+      {
+        tech:"DHT11-sensor",
+        color:techs.DHT11
+      },
+      {
+        tech:'pH-sensor',
+        color:techs["pH sensor"]
+      }
+    ]
+  },
+  {
+    id: 3,
+  },
   { 
-    id: 2, 
+    id: 4, 
+    title: "CheckMate Academy", 
+    imageUrl: project_chess,
+    about:"Chess game with puzzles, tutorials, AI modes. Features hint provision, diverse modes, and real-time game evaluation for enhanced strategic decision-making and engaging user experience.",
+    technologies:[
+      {
+        tech:'Python',
+        color:techs.Python
+      },
+      {
+        tech:'Pygames',
+        color:techs.Pygame
+      },
+      {
+        tech:'Stockfish',
+        color:techs.Stockfish
+      },
+    ]
+  },
+  {
+    id: 5,
+    title: "Portfolio",
+    imageUrl:project_portfolio,
+    about:"Discover more about me, explore my portfolio, and feel free to reach out. Let's connect and create something amazing together!",
+    technologies:[
+      {
+        tech:'React',
+        color:techs.React
+      },
+      {
+        tech:'framer-motion',
+        color:techs["Framer Motion"]
+      },
+      {
+        tech:'Vite',
+        color:techs.Vite
+      },
+    ]
+  },
+  { 
+    id: 6, 
     title: "GreInsights", 
     imageUrl: project_gre ,
     about:"This app analyzes GRE scores, offers Ivy League recommendations based on 5-year admission trends. Ideal for college-bound students seeking guidance.",
@@ -85,27 +162,7 @@ const projects = [
     ]
   },
   { 
-    id: 3, 
-    title: "CheckMate Academy", 
-    imageUrl: project_chess,
-    about:"Chess game with puzzles, tutorials, AI modes. Features hint provision, diverse modes, and real-time game evaluation for enhanced strategic decision-making and engaging user experience.",
-    technologies:[
-      {
-        tech:'Python',
-        color:techs.Python
-      },
-      {
-        tech:'Pygames',
-        color:techs.Pygame
-      },
-      {
-        tech:'Stockfish',
-        color:techs.Stockfish
-      },
-    ]
-  },
-  { 
-    id: 4, 
+    id: 7, 
     title: "Railway Management System", 
     imageUrl: project_railway,
     about:"Java-based Railway Reservation System for Admins to manage trains and Users to book tickets, utilizing OOP principles.",
@@ -117,7 +174,7 @@ const projects = [
     ]
   },
   { 
-    id: 5, 
+    id: 8, 
     title: "Learn4U", 
     imageUrl: project_learn4u,
     about:"Online learning platform providing courses in programming, software development, physical education, and diverse subjects for comprehensive learning.",
@@ -136,6 +193,18 @@ const projects = [
       },
     ]
   },
+  {
+    id: 9,
+    title:"BattleShip Board Game",
+    imageUrl:project_battleship,
+    about:"A Battleship Board Game implemented from scratch using C language",
+    technologies:[
+      {
+        tech:'C',
+        color:techs.C
+      }
+    ]
+  }
 ];
 
 export default projects;
