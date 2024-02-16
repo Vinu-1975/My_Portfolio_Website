77cee99fc2fb71f92eca762d0c5720317c131fff
vinayakanvs <vinayakanvs2003@gmail.com>
2024-02-17 00:46:35 +0530

    made contact forms as controlled input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,32 @@
+import { useState } from "react";
+
 function Contact() {
+  // template_46s82fn
+  // service_48l89pu
+  // 2mPdkHPhIbKBMHWMQ
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+    console.log(formData);
+    // You can add your logic to submit the form data
+  };
+
   return (
     <section className="contact" id="contact">
       <div className="container">
@@ -8,28 +36,51 @@ function Contact() {
         </div>
         <div className="form-container">
           <div className="login-box">
-            <form>
-              <div className="user-box" style={{marginTop:'10px'}}>
-                <input type="text" name="" required />
+            <form onSubmit={handleSubmit}>
+              <div className="user-box" style={{ marginTop: "10px" }}>
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
                 <label>Your Name</label>
               </div>
               <div className="user-box">
-                <input type="email" name="" required />
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
                 <label>Your email</label>
               </div>
               <div className="user-box">
-                <input type="email" name="" required />
+                <input
+                  type="text"
+                  name="subject"
+                  value={formData.subject}
+                  onChange={handleChange}
+                  required
+                />
                 <label>Subject</label>
               </div>
               <div className="user-box">
-                <textarea name="" required></textarea>
+                <textarea
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                ></textarea>
                 <label>Message</label>
               </div>
-              <center style={{marginTop:'-60px'}}>
-                <a href="#">
+              <center style={{ marginTop: "-60px" }}>
+                <button type="submit">
                   SEND
                   <span></span>
-                </a>
+                </button>
               </center>
             </form>
           </div>
